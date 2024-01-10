a73f5ef3c3d8a776608c3bbb137e57d12af45698
kanaru-ssk <2019january.0131@gmail.com>
2024-01-10 23:39:46 +0900

    add link to footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,13 +1,17 @@
 <footer
-  class="bg-primary-500 space-y-0.5 p-8 text-base text-white md:px-24 md:py-12"
+  class="bg-primary-500 space-y-0.5 p-8 text-base font-bold text-white md:px-24 md:py-12"
 >
   <p>卒業研究・制作展事務局</p>
-  <p>myu_sotsuten@myu.ac.jp</p>
-  <p>022-377-8332</p>
+  <p>
+    <a href="mailto:myu_sotsuten@myu.ac.jp" class="underline"
+      >myu_sotsuten@myu.ac.jp</a
+    >
+  </p>
+  <p><a href="tel:022-377-8332" class="underline">022-377-8332</a></p>
   <p>
     <a href="http://myu-design.jp/" class="underline">
       宮城大学デザインポータルHP
     </a>
   </p>
-  <p>MYU Design&copy;2021</p>
+  <p>MYU Design&copy;2023</p>
 </footer>
