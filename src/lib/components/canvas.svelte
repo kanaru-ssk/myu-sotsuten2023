f180764d9ec6def0fef0a00faee82363ef16bc5b
kanaru-ssk <2019january.0131@gmail.com>
2024-01-14 09:34:32 +0900

    使っていないcanvas削除

--- src/lib/components/canvas.svelte
+++ src/lib/components/canvas.svelte
@@ -1,35 +1,3 @@
-<script>
-  import { onMount } from "svelte";
-
-  let canvas;
-
-  onMount(() => {
-    // const ctx = canvas.getContext("2d");
-    // if (!ctx) return;
-    // const width = canvas.width;
-    // const height = canvas.height;
-    // let r = 0;
-    // (function loop() {
-    //   ctx.clearRect(0, 0, width, height);
-    //   r += 0.01;
-    //   ctx.save();
-    //   ctx.translate(width / 2, height / 2);
-    //   ctx.rotate(r);
-    //   ctx.fillStyle = "#5EB9D7";
-    //   ctx.fillRect(-200, -200, 400, 400);
-    //   ctx.restore();
-    //   requestAnimationFrame(loop);
-    // })();
-  });
-</script>
-
-<canvas
-  bind:this={canvas}
-  width={2560}
-  height={1600}
-  class="fixed -z-10 h-svh w-screen"
-/>
-
 <!-- ローディングアニメ―ション -->
 <div id="load">
   <div class="load-main">
